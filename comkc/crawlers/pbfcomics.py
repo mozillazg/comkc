85d092baf58faf6dd5c1b91d35d307b287b80eb5
mozillazg <mozillazg101@gmail.com>
2020-11-28 19:57:19 +0800

    add loadingartist and pbfcomics

---
+++ comkc/crawlers/pbfcomics.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from pyquery import PyQuery as pq
+
+from comkc.crawlers import BaseWorker
+
+logger = logging.getLogger(__name__)
+
+
+class Worker(BaseWorker):
+    SITE = 'he Perry Bible Fellowship'
+    BASE_URL = 'https://pbfcomics.com/feed/'
+    ENABLE = True
+
+    async def get_items(self):
+        html = await self.fetch_url(self.BASE_URL)
+        data = await self.parse_rss_items(html)
+
+        for item in data:
+            title = item['title']
+            item.update({
+                'title': '{0}: {1}'.format(self.SITE, title),
+                'url': item['link'],
+                'date': item['pubDate'],
+            })
+        return data
+
+    async def parse_item(self, url):
+        html = await self.fetch_url(url)
+        image = pq(html)('#comic img').attr('data-src')
+        return {'image': image}
+
+
+if __name__ == '__main__':
+    import asyncio
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(Worker().run())
+    finally:
+        loop.close()
